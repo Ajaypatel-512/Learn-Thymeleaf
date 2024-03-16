d84150757025e421cf0984bc56a2fb97b35d7e8d
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-03-16 11:44:48 +0530

    Iteration in thymeleaf

--- src/main/java/com/course/thymleaf/controller/MainController.java
+++ src/main/java/com/course/thymleaf/controller/MainController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -15,16 +18,38 @@ public class MainController {
 
     @GetMapping("/elvis")
     public String elvisExample(Model model){
+
+        /* Conditional Statements
+         * Elvis operator  ? :
+         * If unless
+         * switch case
+         * */
+
         model.addAttribute("isAdmin",true);
         model.addAttribute("gender","M");
         return "elvis";
     }
 
-    /* Conditional Statements
-     * Elvis operator  ? :
-     * If unless
-     * switch case
-     * */
+
+    @GetMapping("/each")
+    public String eachExample(Model model){
+
+        /*
+         * Iteration
+         *
+         * Thymeleaf provides a status variable for us to keep track of the process. Status variable has the following properties which are very useful
+         * index: the current iteration index, starting with 0 (zero)
+         * count: the number of elements processed so far
+         * size: the total number of elements in the list
+         * even/odd: checks if the current iteration index is even or odd
+         * first: checks if the current iteration is the first one
+         * last: checks if the current iteration is the last one
+         * */
+
+        List<String> stringList = List.of("First","Second","Third");
+        model.addAttribute("list",stringList);
+        return "each";
+    }
 
 
 
